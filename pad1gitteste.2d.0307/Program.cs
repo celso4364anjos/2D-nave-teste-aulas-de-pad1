0378327b12091172105d366ca239a41aa7add626
celso4364anjos <samuel.celso.anjos@gmail.com>
2019-03-07 14:51:11 -0300

    primeira versao do projeto

---
+++ pad1gitteste.2d.0307/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pad1gitteste._2d._0307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lindo sou eu :)");
+        }
+    }
+}
